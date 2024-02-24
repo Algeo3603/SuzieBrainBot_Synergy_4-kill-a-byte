e7e334dd347ff15cd8afc715e59c13578d889a40
Algeo3603 <alangeorge3603@gmail.com>
2024-02-24 23:29:43 +0530

    ui

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Suicide Help", layout="wide")
+messages = [
+    {
+        "role": "system",
+        "content": "You are a friendly chatbot who always responds in the style of a psychiatrist. Give the user specififc steps and suggestions to improve their condition",
+    },
+    {"role": "user", "content": "I am feeling lonely, whom should I approach?"},
+]
+
+
+nav_pages = ['Regi', 'Converse']
+selected_page = st.sidebar.selectbox("Navigate: ", nav_pages)
+
+if selected_page == 'Regi':
+    st.title("Regi page")
+    st.write("Enter your details")
+elif selected_page == 'Converse':
+    st.title("Chatbot")
+    st.write("You suck, you should suicide")
+
+    chat_ui = st.container(height=400)
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+ig
+# from transformers import AutoModelForCausalLM
+
+# config = PeftConfig.from_pretrained("givyboy/TinyLlama-1.1B-Chat-v1.0-mental-health-conversational")
+# model = AutoModelForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
+# model = PeftModel.from_pretrained(model, "givyboy/TinyLlama-1.1B-Chat-v1.0-mental-health-conversational")
+
