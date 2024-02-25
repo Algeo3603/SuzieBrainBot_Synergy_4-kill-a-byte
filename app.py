de589fbd1d32a3cd19f8066f2f2b2ea84f337d56
Algeo3603 <alangeorge3603@gmail.com>
2024-02-25 11:43:19 +0530

    complete

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import torch
 from transformers import pipeline
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.model_selection import train_test_split
 
 pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16, device_map="auto")
 selected_options = []
@@ -14,6 +21,52 @@ st.session_state.messages = [
     # {"role": "user", "content": "How can I suicide?"},
 ]
 
+def func(path):
+    dfr=pd.read_csv(path)
+    df = pd.read_csv("EEG.machinelearing_data_BRMH.csv")
+    df1 = df.filter(regex='^(?!.*COH)')
+    dfr = dfr.filter(regex='^(?!.*COH)')
+    columns_to_remove = ['education', 'date', 'Unnamed: 122','no.','eeg.date']
+    df2 = df1.drop(columns=columns_to_remove, errors='ignore')
+    dfr = dfr.drop(columns=columns_to_remove, errors='ignore')
+    df3 = df2.dropna()
+    df3['sex'] = df3['sex'].replace({'M': 0, 'F': 1})
+    dfr['sex'] = dfr['sex'].replace({'M': 0, 'F': 1})
+    df3=df3.drop(['specific.disorder'],axis=1)
+    dfr=dfr.drop(['specific.disorder'],axis=1)
+
+
+    # Assuming df is your DataFrame containing the data
+    X = df3.drop(columns=['main.disorder'])  # Features
+
+    # Standardize the features
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+
+    pca = PCA(n_components=20)
+    X_pca = pca.fit_transform(X_scaled)
+    y=df3['main.disorder']
+    X_train=X_pca
+    y_train=y
+    # X_test=X_pca
+    # y_test=y
+    X_train, X_test1, y_train, y_test1 = train_test_split(X_pca, y, test_size=0.1, random_state=42)
+    X_test, X_test2, y_test, y_test1 = train_test_split(X_pca, y, test_size=0.1, random_state=45)
+
+    # Train a Random Forest classifier
+    rf_classifier = RandomForestClassifier()
+    rf_classifier.fit(X_train, y_train)
+    rf_accuracy = rf_classifier.score(X_test, y_test)
+
+
+    X = dfr.drop(columns=['main.disorder'])  # Features
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    pca_data = pca.transform(X)
+
+    predicted_labels = rf_classifier.predict(pca_data) 
+    return predicted_labels
 
 st.set_page_config(page_title="Suicide Help", layout="wide")
 
@@ -93,4 +146,8 @@ elif selected_page == 'Converse with Suzie':
 elif selected_page == 'EEG analysis':
     st.title("EEG analysis")
 
-    
+    path = st.file_uploader("Upload a CSV", type=['csv'])
+
+    if path is not None:
+        predicted_labels = func(path)
+        st.write("You probably have the following condition: ", predicted_labels[0])
